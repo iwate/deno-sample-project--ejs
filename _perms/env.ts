e5ac59c1629f1cb005f3e3fb75abe7ab5f542ca9
yoshiyuki taniguchi <taniguchi@commerble.com>
2024-07-31 12:01:50 +0900

    chore: use helper func insted of console.log

--- _perms/env.ts
+++ _perms/env.ts
@@ -1,8 +1,10 @@
-console.log([
+import { It_needs } from "./_shared.ts";
+
+It_needs(
   'DENO_DIR',
   'LOCALAPPDATA',
   'DENO_AUTH_TOKENS',
   'DENO_REGISTRY_URL',
   'ESBUILD_BINARY_PATH',
-  'SASS_PATH',
-].join(','))
+  'SASS_PATH'
+)
